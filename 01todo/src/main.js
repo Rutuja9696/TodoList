381c4f50494dd51878c8410284e13f3889086148
Rutuja9696 <rutuja.patil@contentstact.com>
2020-11-29 16:39:58 +0530

    added delete button and its function

--- 01todo/src/main.js
+++ 01todo/src/main.js
@@ -4,10 +4,21 @@ const formSubmission = (event) => {
   event.preventDefault();
   console.log("Task Name=", document.forms.taskList.task.value);
   var item = document.getElementById("task").value;
-  var text = document.createTextNode(item);
+
+  // var text = document.createTextNode(item);
+  var deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "Delete";
+  //task added will be displayed as list
   var li = document.createElement("li");
-  li.appendChild(text);
+  li.innerHTML = item;
+
+  li.appendChild(deleteButton);
   document.getElementById("todoList").appendChild(li);
+
+  deleteButton.addEventListener("click", () => {
+    li.classList.add("delete");
+  });
+  //
 
   // if(isStringEmpty && isMarkup){
   //   DocumentFragment.getElementById("todoList").appendChild(())
