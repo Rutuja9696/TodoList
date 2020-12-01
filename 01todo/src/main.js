deb5bf711340141f22016cac60aac77f40736ccc
Rutuja9696 <rutuja.patil@contentstact.com>
2020-12-01 10:34:46 +0530

    added styles

--- 01todo/src/main.js
+++ 01todo/src/main.js
@@ -5,22 +5,20 @@ const formSubmission = (event) => {
   console.log("Task Name=", document.forms.taskList.task.value);
   var item = document.getElementById("task").value;
 
-  // var text = document.createTextNode(item);
   var deleteButton = document.createElement("button");
   deleteButton.innerHTML = "Delete";
   //task added will be displayed as list
   var li = document.createElement("li");
   li.innerHTML = item;
-
+  //list will be displayed aling with delete button
   li.appendChild(deleteButton);
   document.getElementById("todoList").appendChild(li);
 
   deleteButton.addEventListener("click", () => {
     li.classList.add("delete");
   });
-  //
 
   // if(isStringEmpty && isMarkup){
-  //   DocumentFragment.getElementById("todoList").appendChild(())
+  //   Document.getElementById("todoList").appendChild(())
   //   event.target..value=""
 };
