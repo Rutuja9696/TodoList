3ebc9d51d0b21c6bd30676e88bae30738289b945
Rutuja9696 <rutuja.patil@contentstact.com>
2020-12-01 14:51:08 +0530

    added line through and done button

--- 01todo/src/main.js
+++ 01todo/src/main.js
@@ -5,15 +5,23 @@ const formSubmission = (event) => {
   console.log("Task Name=", document.forms.taskList.task.value);
   var item = document.getElementById("task").value;
 
+  var doneButton = document.createElement("button");
+  doneButton.innerHTML = "Done";
   var deleteButton = document.createElement("button");
   deleteButton.innerHTML = "Delete";
+
   //task added will be displayed as list
   var li = document.createElement("li");
   li.innerHTML = item;
   //list will be displayed aling with delete button
+  li.appendChild(doneButton);
+  document.getElementById("todoList").appendChild(li);
   li.appendChild(deleteButton);
   document.getElementById("todoList").appendChild(li);
 
+  doneButton.addEventListener("click", () => {
+    li.classList.add("done");
+  });
   deleteButton.addEventListener("click", () => {
     li.classList.add("delete");
   });
