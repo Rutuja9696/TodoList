0d568132e15db2f0695fe8ded3b573d1a8173b45
Rutuja9696 <rutuja.patil@contentstact.com>
2020-12-01 15:28:15 +0530

    added alignment to the output

--- 01todo/src/main.js
+++ 01todo/src/main.js
@@ -4,7 +4,7 @@ const formSubmission = (event) => {
   event.preventDefault();
   console.log("Task Name=", document.forms.taskList.task.value);
   var item = document.getElementById("task").value;
-
+  //creating buttons
   var doneButton = document.createElement("button");
   doneButton.innerHTML = "Done";
   var deleteButton = document.createElement("button");
@@ -13,12 +13,14 @@ const formSubmission = (event) => {
   //task added will be displayed as list
   var li = document.createElement("li");
   li.innerHTML = item;
-  //list will be displayed aling with delete button
+
+  //list will be displayed along with done and delete button
   li.appendChild(doneButton);
   document.getElementById("todoList").appendChild(li);
   li.appendChild(deleteButton);
   document.getElementById("todoList").appendChild(li);
 
+  //on click function for done and delete buttons
   doneButton.addEventListener("click", () => {
     li.classList.add("done");
   });
